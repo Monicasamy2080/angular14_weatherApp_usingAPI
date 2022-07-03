89572dc9ec0302866cabf09f0144075220312095
monica <monicasamy2080@gmail.com>
2022-07-03 13:30:57 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  implements OnInit{
+
+  constructor(private weatherService:WeatherService){}
+  cityName:string='wellington';
+  weatherData?:WeatherData;
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName='';
+
+
+  }
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName='';
+
+
+  }
+
+  private getWeatherData(cityName:string){
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next:(Response)=>{
+        this.weatherData=Response;
+        console.log(Response);
+      }
+    });
+  }
+}
